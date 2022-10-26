5dc118849a43f6ef08eb43ea9e0c3034149c1750
Eryk Latasiewicz <eryk.ne05@gmail.com>
2022-10-26 19:30:44 +0200

    update

---
+++ dist/main.js
@@ -0,0 +1,23 @@
+const passwordInput = document.querySelector("#password-generated input"),
+  generate_btn = document.querySelector("#generate-btn"),
+  range_num = document.querySelector("#range-number"),
+  range = document.querySelector("#range input");
+
+const allcharacters =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*[]{}():;',.<>/?|-_=+";
+
+const generatepassword = () => {
+  let password = "";
+
+  for (let i = 0; i < range.value; i++) {
+    let random = Math.floor(Math.random() * allcharacters.length);
+    password += allcharacters[random];
+  }
+  passwordInput.value = password;
+};
+
+range.addEventListener("input", () => {
+  range_num.innerText = range.value;
+  generatepassword();
+});
+generate_btn.addEventListener("click", generatepassword);
